a6a48d0e50f2a1f11de29046b271cb72ef5630d4
Ritik Suthar <ritiksuthar.prydan@gmail.com>
2026-02-02 11:52:46 +0530

    Add new backend folders

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { connectToMongoDB } = require('./connect');
+
+const app = express();
+const PORT = 3000;
+
+connectToMongoDB("mongodb://127.0.0.1:27017/College-ERP").then(() =>
+  console.log("MongoDB connected"),
+);
+
+app.get('/', (req,res) =>{
+    res.send('Hello from server...')
+})
+
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`))
